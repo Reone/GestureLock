5533d852d73a642877da8bb49763baa9811dd400
wangxingsheng <164587694@qq.com>
2018-03-12 10:50:32 +0800

    add demoActivity

---
+++ simple/src/main/java/com/reone/simple/DemoActivity.java
@@ -0,0 +1,17 @@
+package com.reone.simple;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wangxingsheng on 2018/2/28.
+ */
+
+public class DemoActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
